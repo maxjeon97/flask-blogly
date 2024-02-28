c55e85a9777ab7748edee3b547cc73b37a7d8d0a
Max Jeon <maxjeon97@gmail.com>
2024-02-28 12:10:39 -0800

    create user model and base html

--- app.py
+++ app.py
@@ -2,8 +2,10 @@
 
 import os
 
-from flask import Flask
-from models import connect_db
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import connect_db, User, db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
@@ -12,3 +14,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+@app.get('/')
+def display_list_of_users():
+    return redirect('/users')
+
+@app.get('/users')
+def show_users():
+    return render_template('users.html', users=users)
+
--- models.py
+++ models.py
@@ -1,1 +1,39 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """Users. A site has many users"""
+
+    __tablename__ = "users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    first_name = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    last_name = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    image_url = db.Column(
+        db.String(50),
+        nullable=False, # could add a default image URL
+        default="image.jpg"
+    )
